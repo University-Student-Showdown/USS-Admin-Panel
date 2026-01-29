0b13a0d9641cbcdcdf3e40a6ee06ea387b1133b2
madsies <madsiesss@gmail.com>
2026-01-29 18:16:41 +0000

    Initial refactor completed

---
+++ src/main/java/com/uss/madsies/managers/RoundManager.java
@@ -0,0 +1,20 @@
+package com.uss.madsies;
+
+import com.uss.madsies.data.MatchUp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RoundManager
+{
+    public List<MatchUp> matches = new ArrayList<>();
+    public boolean isCurrentMatch = false;
+
+    public RoundManager()
+    {
+
+
+    }
+
+    
+}
